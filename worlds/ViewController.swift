4c268965ba1408505b02de90598d8baf4d203a28
Johan Halin <johan@halin.me>
2017-10-17 16:53:47 +0300

    Initial Commit

---
+++ worlds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  worlds
+//
+//  Created by Johan Halin on 17/10/2017.
+//  Copyright © 2017 Dekadence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
