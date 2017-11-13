e6761db435a33109084e91384716fe849213820c
Johan Halin <johan@halin.me>
2017-11-13 19:51:09 +0200

    A bunch of stuff.
    - Removed main storyboard, create the view controller in code instead.
    - Start button.
    - After the button has faded out, the music starts playing.

--- worlds/AppDelegate.swift
+++ worlds/AppDelegate.swift
@@ -10,12 +10,13 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = ViewController.init()
+        self.window?.makeKeyAndVisible()
+
         return true
     }
 
--- worlds/ViewController.swift
+++ worlds/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    let audioPlayer: AVAudioPlayer
+    let startButton: UIButton
+    
+    // MARK: Private
+    
+    @objc private func startButtonTouched(button: UIButton) {
+        UIView.animate(withDuration: 1, animations: {
+            self.startButton.alpha = 0
+        }, completion: { _ in
+            self.startButton.removeFromSuperview()
+            self.start()
+        })
+    }
+    
+    private func start() {
+        self.audioPlayer.play()
+    }
+    
+    // MARK: UIViewController
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.black
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.startButton.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func prefersHomeIndicatorAutoHidden() -> Bool {
+        return true
+    }
+    
+    init() {
+        if let trackUrl = Bundle.main.url(forResource: "track", withExtension: "m4a") {
+            guard let audioPlayer = try? AVAudioPlayer(contentsOf: trackUrl) else {
+                abort()
+            }
+            
+            self.audioPlayer = audioPlayer
+            self.audioPlayer.prepareToPlay()
+        } else {
+            abort()
+        }
+        
+        self.startButton = UIButton.init(type: UIButtonType.custom)
+        self.startButton.setTitle("start", for: UIControlState.normal)
 
-
+        super.init(nibName: nil, bundle: nil)
+        
+        self.startButton.addTarget(self, action: #selector(startButtonTouched), for: UIControlEvents.touchUpInside)
+        self.view.addSubview(self.startButton)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
-
