29d74ff9f7e68b3adff5af237052b92af24c0b25
Johan Halin <johan@halin.me>
2017-11-16 19:42:04 +0200

    Skybox type thing, consisting of two overlapping boxes.
    Should have a texture, now it's just two shitty colors.

--- worlds/ViewController.swift
+++ worlds/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     let startButton: UIButton
     let sceneView: SCNView
     let camera: SCNNode
+    let skyBoxes: [SCNNode]
     
     var boxes: [SCNNode]
 
@@ -65,6 +66,19 @@ class ViewController: UIViewController {
 
         moveCamera()
         rotateSphereBoxes()
+        
+        for skybox in self.skyBoxes {
+            skybox.runAction(
+                SCNAction.repeatForever(
+                    SCNAction.rotateBy(
+                        x: CGFloat(-10 + Int(arc4random_uniform(20))),
+                        y: CGFloat(-10 + Int(arc4random_uniform(20))),
+                        z: CGFloat(-10 + Int(arc4random_uniform(20))),
+                        duration: TimeInterval(18 + arc4random_uniform(5))
+                    )
+                )
+            )
+        }
     }
     
     // MARK: UIViewController
@@ -93,8 +107,25 @@ class ViewController: UIViewController {
         scene.rootNode.addChildNode(omniLightNode)
         
         scene.rootNode.addChildNode(self.camera)
-//        self.sceneView.allowsCameraControl = true
 
+        var i = 0 // this whole thing is for debugging purposes. should be a texture actually or something
+        
+        for skyboxNode in self.skyBoxes {
+            guard let skybox = skyboxNode.geometry as! SCNBox? else { abort() }
+            
+            let length = CGFloat(500)
+            skybox.width = length
+            skybox.height = length
+            skybox.length = length
+            
+            skybox.firstMaterial?.diffuse.contents = i == 0 ? UIColor.green : UIColor.red
+            skybox.firstMaterial?.isDoubleSided = true
+            
+            scene.rootNode.addChildNode(skyboxNode)
+            
+            i += 1
+        }
+        
         let boxCount = 32
         
         for i in 0...boxCount {
@@ -164,12 +195,14 @@ class ViewController: UIViewController {
         
         self.camera = SCNNode()
         let camera = SCNCamera()
-        camera.zFar = 300
+        camera.zFar = 600
         camera.vignettingIntensity = 1
         camera.vignettingPower = 1
         self.camera.camera = camera // lol
         self.camera.position = SCNVector3Make(0, 0, 58)
-
+        
+        self.skyBoxes = [ SCNNode(geometry: SCNBox()), SCNNode(geometry: SCNBox()) ]
+        
         super.init(nibName: nil, bundle: nil)
         
         self.view.addSubview(self.sceneView)
