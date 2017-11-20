3762dca464132e9f7c32319a3aef2e1699b22fa6
Johan Halin <johan@halin.me>
2017-11-20 19:44:06 +0200

    Removed commented and unused code.

--- worlds/ViewController.swift
+++ worlds/ViewController.swift
@@ -190,19 +190,10 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // FIXME: temporary
-//        self.startButton.isHidden = true
         self.startButton.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
-        
         self.sceneView.frame = CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height)
     }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-//        self.start()
-    }
-    
+        
     override func prefersHomeIndicatorAutoHidden() -> Bool {
         return true
     }
